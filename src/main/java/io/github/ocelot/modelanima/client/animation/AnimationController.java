f115c8c53053dc478657a4295f82034b2529bf63
Ocelot <FinntheRaider@gmail.com>
2020-09-16 20:42:08 -0700

    Create AnimationController.java

---
+++ src/main/java/io/github/ocelot/modelanima/client/animation/AnimationController.java
@@ -0,0 +1,10 @@
+package io.github.ocelot.modelanima.client.animation;
+
+public class AnimationController
+{
+    private AnimationState defaultState;
+
+    public static class AnimationState {
+        
+    }
+}
