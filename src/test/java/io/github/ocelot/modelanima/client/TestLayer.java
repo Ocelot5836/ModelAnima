70193bebc548a86bd239cc6e7a88eb2caddee6c1
Ocelot <FinntheRaider@gmail.com>
2020-11-06 15:32:31 -0800

    Epic

--- src/test/java/io/github/ocelot/modelanima/client/TestLayer.java
+++ src/test/java/io/github/ocelot/modelanima/client/TestLayer.java
@@ -29,8 +29,12 @@ public class TestLayer extends LayerRenderer<AbstractClientPlayerEntity, PlayerM
     @Override
     public void render(MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight, AbstractClientPlayerEntity player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch)
     {
-        GeometryModel model = LocalGeometryModelLoader.getModel(new ResourceLocation(TestMod.MOD_ID, "slabfish"));
-        GeometryModelTextureTable texture = GeometryTextureManager.get(new ResourceLocation(TestMod.MOD_ID, "slabfish"));
+        GeometryModel model = LocalGeometryModelLoader.getModel(new ResourceLocation(TestMod.MOD_ID, "button"));
+        GeometryModelTextureTable texture = GeometryTextureManager.get(new ResourceLocation(TestMod.MOD_ID, "button"));
+//        GeometryModelTextureTable backpack = GeometryTextureManager.get(new ResourceLocation(TestMod.MOD_ID, "slabfish_backpack"));
+//        GeometryModelTextureTable sweater = GeometryTextureManager.get(new ResourceLocation(TestMod.MOD_ID, "slabfish_sweater"));
         GeometryModelRenderer.render(this.getEntityModel(), model, texture, matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
+//        GeometryModelRenderer.render(this.getEntityModel(), model, backpack, matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
+//        GeometryModelRenderer.render(this.getEntityModel(), model, sweater, matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
     }
 }
