3040d708b411d743b62c08adc7b103cda41a22ff
Ocelot <FinntheRaider@gmail.com>
2021-01-16 15:03:42 -0800

    :sparkles: epic bug testing

--- src/test/java/io/github/ocelot/modelanima/client/TestLayer.java
+++ src/test/java/io/github/ocelot/modelanima/client/TestLayer.java
@@ -5,6 +5,7 @@ import io.github.ocelot.modelanima.TestMod;
 import io.github.ocelot.modelanima.api.client.geometry.GeometryModelRenderer;
 import io.github.ocelot.modelanima.api.client.geometry.LocalGeometryModelLoader;
 import io.github.ocelot.modelanima.api.client.texture.GeometryTextureManager;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
@@ -12,6 +13,7 @@ import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.PlayerModel;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class TestLayer extends LayerRenderer<AbstractClientPlayerEntity, PlayerModel<AbstractClientPlayerEntity>>
 {
@@ -26,7 +28,18 @@ public class TestLayer extends LayerRenderer<AbstractClientPlayerEntity, PlayerM
         if (GeometryTextureManager.isReloading())
             return;
 
-        GeometryModelRenderer.render(this.getEntityModel(), LocalGeometryModelLoader.getModel(new ResourceLocation(TestMod.MOD_ID, "slope")), new ResourceLocation(TestMod.MOD_ID, "slope"), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
+        Minecraft.getInstance().getRenderTypeBuffers().getBufferSource().finish();
+
+        matrixStack.push();
+        matrixStack.translate(0, 1, 0);
+        matrixStack.scale(0.01f, 0.01f, 0.01f);
+        GeometryModelRenderer.render(this.getEntityModel(), LocalGeometryModelLoader.getModel(new ResourceLocation(TestMod.MOD_ID, "rosalina")), new ResourceLocation(TestMod.MOD_ID, "rosalina"), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
+        matrixStack.pop();
+
+        GL11.glShadeModel(GL11.GL_SMOOTH);
+        Minecraft.getInstance().getRenderTypeBuffers().getBufferSource().finish();
+        GL11.glShadeModel(GL11.GL_FLAT);
+
 //        GeometryModelRenderer.render(this.getEntityModel(), LocalGeometryModelLoader.getModel(new ResourceLocation(TestMod.MOD_ID, "battlefields/hoodie")), new ResourceLocation(TestMod.MOD_ID, "discord_hoodie"), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
 //        GeometryModelRenderer.render(this.getEntityModel(), LocalGeometryModelLoader.getModel(new ResourceLocation(TestMod.MOD_ID, "battlefields/cap")), new ResourceLocation(TestMod.MOD_ID, "v_cap"), matrixStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f);
     }
