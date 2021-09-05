242be8bfee1c2018a267f53c0b8d6a04c6f121ed
Ocelot <FinntheRaider@gmail.com>
2021-09-04 19:56:34 -0700

    :zap: Optimized model rendering

--- src/test/java/io/github/ocelot/modelanima/client/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package io.github.ocelot.modelanima.client;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
